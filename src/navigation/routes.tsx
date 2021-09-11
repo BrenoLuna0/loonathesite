1d9a6ed8dca94d969b8e387d309765fca1b8cf5f
BrenoLuna0 <breno.luna_@hotmail.com>
2021-09-11 20:48:15 -0300

    configuration finished

---
+++ src/navigation/routes.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "../pages/Home";
+
+export default function Routes() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="title-container">
+        <h2>LOONA THE SITE</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
